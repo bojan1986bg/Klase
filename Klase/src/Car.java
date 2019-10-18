a5968b25f745b004d346efc5990f2138ed3bd2aa
okkursevi10 <okkursevi10@UC5PC6>
2019-10-18 21:29:34 +0200

    klase poceli

---
+++ Klase/src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car {
+
+	
+	public String boja;
+	public String marka;
+	public String model;
+	public int cena;
+	public double kubikaza;
+	public boolean daljinskoZakljucavanje;
+	
+	
+}
